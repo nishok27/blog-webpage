fc1c588626190c68a02923abd5aa4be745d0a661
NISHOK R R <56576788+nishok27@users.noreply.github.com>
2023-07-05 16:23:09 +0530

    Add files via upload

---
+++ src/useFetch.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const abortCont = new AbortController();
+
+    setTimeout(() => {
+      fetch(url, { signal: abortCont.signal })
+        // res means response
+        .then((res) => {
+          if (!res.ok) {
+            throw Error(
+              "Could not fetch the data, Please try to connect the server."
+            );
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          if (err.name === "AbortError") {
+            console.log("fetch aborted");
+          } else {
+            setError(err.message);
+            setIsPending(false);
+          }
+        });
+    }, 500);
+    return () => abortCont.abort();
+  }, [url]);
+  return { data, isPending, error };
+};
+
+export default useFetch;
